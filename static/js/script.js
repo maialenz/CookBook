44cbbac1c47e2f298c0f365e4158f6210678baee
maialenz <maialenzumeta@gmail.com>
2021-09-08 20:03:19 +0000

    Add input fields on modal for adding tasks

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
     $('.tooltipped').tooltip();
+    $(".collapsible").collapsible();
+    $(".dropdown-trigger").dropdown();
+    $('.modal').modal();
+    $('select').formSelect();
   });
         
