d016a10dcc7465ba7b05caa362fbf52d733048f4
maialenz <maialenzumeta@gmail.com>
2021-09-10 13:55:36 +0000

    add custom materialize select validation

--- static/js/script.js
+++ static/js/script.js
@@ -5,5 +5,40 @@ $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('.modal').modal();
     $('select').formSelect();
-  });
+
+    /*
+     Taken from Task Manager lesson. If the user doesn't select one of the options from the dropdown, the red color will be applied. If the user chooses an option,
+     the green color will be applied as form validation. If the user tries to submit the form without selecting an option, the user will be notified that the field is
+     required.
+     */
+
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {
+        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+        }
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                    $(this).children("input").css(classValid);
+                }
+            });
+        }).on("click", function () {
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                        }
+                    }
+                });
+            }
+        });
+    }
+});
+
         
